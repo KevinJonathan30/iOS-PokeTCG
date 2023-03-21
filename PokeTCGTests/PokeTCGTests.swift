498970afa2978100817a9b70ec372720cb83432b
kevin.jonathan <kevin.jonathan@gdn-commerce.com>
2023-03-21 15:32:29 +0700

    Add Unit test and UITest, and app icon

--- PokeTCGTests/PokeTCGTests.swift
+++ PokeTCGTests/PokeTCGTests.swift
@@ -9,28 +9,156 @@ import XCTest
 @testable import PokeTCG
 
 final class PokeTCGTests: XCTestCase {
+    var homeSut: HomeViewModel!
+    var detailSut: DetailViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        
+        let navigationController = UINavigationController.init()
+        let appCoordinator = AppCoordinator(navigationController: navigationController)
+        let dataService = MockDataService.sharedInstance
+        
+        homeSut = HomeViewModel(appCoordinator: appCoordinator, dataService: dataService)
+        detailSut = DetailViewModel(appCoordinator: appCoordinator, dataService: dataService, pokemonData: PokemonData(id: "", name: "", supertype: "", subtypes: [], level: "", hp: "", types: [], attacks: [], weaknesses: [], resistances: [], retreatCost: [], convertedRetreatCost: 0, set: nil, number: "", artist: "", rarity: "", flavorText: "", nationalPokedexNumbers: [], legalities: nil, images: nil, tcgplayer: nil, cardmarket: nil))
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        homeSut = nil
+        detailSut = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_getCardList() throws {
+        let successExpect = expectation(description: "data1")
+        
+        var pokemonData: [PokemonData] = []
+        var errorMessage: String = ""
+        
+        XCTAssertTrue(pokemonData.isEmpty)
+        XCTAssertTrue(errorMessage.isEmpty)
+        
+        homeSut.getCardList { result in
+            switch result {
+            case .success(let data):
+                pokemonData = data
+            case .failure(let error):
+                errorMessage = error.localizedDescription
+            }
+            
+            if pokemonData.count == 1 && errorMessage.isEmpty {
+                successExpect.fulfill()
+            }
         }
+        
+        wait(for: [successExpect], timeout: 10)
+    }
+    
+    func test_getSearchCardList() throws {
+        let successExpect = expectation(description: "data1")
+        let failExpect = expectation(description: "fail1")
+        
+        var pokemonData: [PokemonData] = []
+        var errorMessage: String = ""
+        
+        XCTAssertTrue(pokemonData.isEmpty)
+        XCTAssertTrue(errorMessage.isEmpty)
+        
+        homeSut.getSearchCardList(query: "Pikachu") { result in
+            switch result {
+            case .success(let data):
+                pokemonData = data
+            case .failure(let error):
+                errorMessage = error.localizedDescription
+            }
+            
+            if pokemonData.count == 2 && errorMessage.isEmpty {
+                successExpect.fulfill()
+            }
+        }
+        
+        homeSut.getSearchCardList(query: "Harry Potter") { result in
+            switch result {
+            case .success(let data):
+                pokemonData = data
+            case .failure(let error):
+                errorMessage = error.localizedDescription
+            }
+            
+            if errorMessage == URLError.invalidResponse.localizedDescription {
+                failExpect.fulfill()
+            }
+        }
+        
+        wait(for: [successExpect, failExpect], timeout: 10)
+    }
+    
+    func test_getRecommendationCardList() throws {
+        let successExpect = expectation(description: "data1")
+
+        var pokemonData: [PokemonData] = []
+        var errorMessage: String = ""
+        
+        XCTAssertTrue(pokemonData.isEmpty)
+        XCTAssertTrue(errorMessage.isEmpty)
+
+        detailSut.getRecommendationCardList { result in
+            switch result {
+            case .success(let data):
+                pokemonData = data
+            case .failure(let error):
+                errorMessage = error.localizedDescription
+            }
+            
+            if pokemonData.isEmpty && errorMessage.isEmpty {
+                successExpect.fulfill()
+            }
+        }
+
+        wait(for: [successExpect], timeout: 10)
     }
 
 }
+
+class MockDataService: NSObject {
+    private override init() { }
+    static let sharedInstance: MockDataService =  MockDataService()
+    private var key: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "PokeTCG-Info", ofType: "plist") else {
+                fatalError("Couldn't find file 'PokeTCG-Info.plist'.")
+            }
+            
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                fatalError("Couldn't find key 'API_KEY' in 'PokeTCG-Info.plist'.")
+            }
+            return value
+        }
+    }
+}
+
+// MARK: API List
+
+extension MockDataService: AppDataServiceProtocol {
+    func getCardList(page: Int = 1, completion: @escaping(Result<[PokemonData], Error>) -> Void) {
+        completion(.success([PokemonData(id: "", name: "", supertype: "", subtypes: [], level: "", hp: "", types: [], attacks: [], weaknesses: [], resistances: [], retreatCost: [], convertedRetreatCost: 0, set: nil, number: "", artist: "", rarity: "", flavorText: "", nationalPokedexNumbers: [], legalities: nil, images: nil, tcgplayer: nil, cardmarket: nil)]))
+    }
+    
+    func getSearchCardList(page: Int = 1, query: String, completion: @escaping(Result<[PokemonData], Error>) -> Void) {
+        if query == "Pikachu" {
+            completion(.success([PokemonData(id: "", name: "", supertype: "", subtypes: [], level: "", hp: "", types: [], attacks: [], weaknesses: [], resistances: [], retreatCost: [], convertedRetreatCost: 0, set: nil, number: "", artist: "", rarity: "", flavorText: "", nationalPokedexNumbers: [], legalities: nil, images: nil, tcgplayer: nil, cardmarket: nil), PokemonData(id: "", name: "", supertype: "", subtypes: [], level: "", hp: "", types: [], attacks: [], weaknesses: [], resistances: [], retreatCost: [], convertedRetreatCost: 0, set: nil, number: "", artist: "", rarity: "", flavorText: "", nationalPokedexNumbers: [], legalities: nil, images: nil, tcgplayer: nil, cardmarket: nil)]))
+        } else {
+            completion(.failure(URLError.invalidResponse))
+        }
+    }
+    
+    func getCardRecommendation(page: Int = 1, types: [String]?, subtypes: [String]?, completion: @escaping (Result<[PokemonData], Error>) -> Void) {
+        if types != nil,
+           subtypes != nil {
+            completion(.success([]))
+        } else {
+            completion(.failure(URLError.invalidResponse))
+        }
+    }
+}
+
--- PokeTCGUITests/PokeTCGUITests.swift
+++ PokeTCGUITests/PokeTCGUITests.swift
@@ -8,34 +8,50 @@
 import XCTest
 
 final class PokeTCGUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func test_clickOnCard() {
+        app.collectionViews.children(matching: .cell).element(boundBy: 3).children(matching: .other).element.tap()
+        
+        let collectionViewsQuery = app.scrollViews.otherElements.collectionViews
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element.tap()
+    }
+    
+    func test_searchPikachu() {
+        let poketcgNavigationBar = app.navigationBars["PokeTCG"]
+        poketcgNavigationBar.buttons["Search"].tap()
+        poketcgNavigationBar/*@START_MENU_TOKEN@*/.searchFields["Search Pokédex"]/*[[".staticTexts[\"PokeTCG\"].searchFields[\"Search Pokédex\"]",".searchFields[\"Search Pokédex\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let pKey = app/*@START_MENU_TOKEN@*/.keys["P"]/*[[".keyboards.keys[\"P\"]",".keys[\"P\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        pKey.tap()
+        
+        let iKey = app/*@START_MENU_TOKEN@*/.keys["i"]/*[[".keyboards.keys[\"i\"]",".keys[\"i\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        iKey.tap()
+        
+        let kKey = app/*@START_MENU_TOKEN@*/.keys["k"]/*[[".keyboards.keys[\"k\"]",".keys[\"k\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        kKey.tap()
+        
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey.tap()
+        
+        let cKey = app/*@START_MENU_TOKEN@*/.keys["c"]/*[[".keyboards.keys[\"c\"]",".keys[\"c\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        cKey.tap()
+        
+        let hKey = app/*@START_MENU_TOKEN@*/.keys["h"]/*[[".keyboards.keys[\"h\"]",".keys[\"h\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        hKey.tap()
+        
+        let uKey = app/*@START_MENU_TOKEN@*/.keys["u"]/*[[".keyboards.keys[\"u\"]",".keys[\"u\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        uKey.tap()
+        
+        app/*@START_MENU_TOKEN@*/.buttons["Search"]/*[[".keyboards",".buttons[\"search\"]",".buttons[\"Search\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 3).children(matching: .other).element.tap()
     }
 }
